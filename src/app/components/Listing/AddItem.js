43b02ae1d883a377d941830e6a915f243ab38ce5
Nicole Chung <nc06279@uga.edu>
2024-04-08 01:04:13 -0400

    form sizes update

--- src/app/components/Listing/AddItem.js
+++ src/app/components/Listing/AddItem.js
@@ -103,12 +103,37 @@ const AddItem = (props) => {
                         <h3 className='subcategory'>Size</h3>
                             <select id="dropdown" name="size" onChange={handleChange}>
                                 <option hidden selected value></option>
-                                <option value="X-Small">X-Small</option>
-                                <option value="Small">Small</option>
-                                <option value="Medium">Medium</option>
-                                <option value="Large">Large</option>
-                                <option value="X-Large">X-Large</option>
-
+                                <optgroup label="Clothing">
+                                <    option value="XX-Small">XX-Small</option>
+                                    <option value="X-Small">X-Small</option>
+                                    <option value="Small">Small</option>
+                                    <option value="Medium">Medium</option>
+                                    <option value="Large">Large</option>
+                                    <option value="X-Large">X-Large</option>
+                                    <option value="XX-Large">XX-Large</option>
+                                    <option value="Other">Other</option>
+                                </optgroup>
+                                <optgroup label="Shoes">
+                                    <option value="5">5</option>
+                                    <option value="5.5">5.5</option>
+                                    <option value="6">6</option>
+                                    <option value="6.5">6.5</option>
+                                    <option value="7">7</option>
+                                    <option value="7.5">7.5</option>
+                                    <option value="8">8</option>
+                                    <option value="8.5">8.5</option>
+                                    <option value="9">9</option>
+                                    <option value="9.5">9.5</option>
+                                    <option value="10">10</option>
+                                    <option value="10.5">10.5</option>
+                                    <option value="11">11</option>
+                                    <option value="11.5">11.5</option>
+                                    <option value="12">12</option>
+                                    <option value="12.5">12.5</option>
+                                    <option value="13">13</option>
+                                    <option value="13.5">13.5</option>
+                                    <option value="Other">Other</option>
+                                </optgroup>
                             </select>
                     </div>
                     <div>
