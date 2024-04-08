7b1c5a7ac170ec4a3b3fbdf7429c74d365c74f6c
Nicole Chung <nc06279@uga.edu>
2024-04-08 00:58:25 -0400

    form newItem fix

--- src/app/components/Listing/AddItem.js
+++ src/app/components/Listing/AddItem.js
@@ -88,43 +88,43 @@ const AddItem = (props) => {
                     <h2 className='subheader'>Info</h2>
                     <div>
                         <h3 className='subcategory'>Category</h3>
-                            <select id="dropdown" name="dropdown" onChange={handleChange}>
+                            <select id="dropdown" name="category" onChange={handleChange}>
                                 <option hidden selected value></option>
-                                <option value="option1">Women's</option>
-                                <option value="option2">Men's</option>
-                                <option value="option3">Home</option>
+                                <option value="Women's">Women's</option>
+                                <option value="Men's">Men's</option>
+                                <option value="Home">Home</option>
                             </select>
                     </div>
                     <div>
                         <h3 className='subcategory'>Brand</h3>
-                            <input id="brand-input" type="text" onChange={handleChange}/>
+                            <input id="brand-input" name="brand" type="text" onChange={handleChange}/>
                     </div>
                     <div>
                         <h3 className='subcategory'>Size</h3>
-                            <select id="dropdown" name="dropdown" onChange={handleChange}>
+                            <select id="dropdown" name="size" onChange={handleChange}>
                                 <option hidden selected value></option>
-                                <option value="option1">X-Small</option>
-                                <option value="option2">Small</option>
-                                <option value="option3">Medium</option>
-                                <option value="option4">Large</option>
-                                <option value="option3">X-Large</option>
+                                <option value="X-Small">X-Small</option>
+                                <option value="Small">Small</option>
+                                <option value="Medium">Medium</option>
+                                <option value="Large">Large</option>
+                                <option value="X-Large">X-Large</option>
 
                             </select>
                     </div>
                     <div>
                         <h3 className='subcategory'>Condition</h3>
-                            <select id="dropdown" name="dropdown" onChange={handleChange}>
+                            <select id="dropdown" name="condition" onChange={handleChange}>
                                 <option hidden selected value></option>
-                                <option value="option1">New</option>
-                                <option value="option2">Used - Great</option>
-                                <option value="option3">Used - Good</option>
-                                <option value="option3">Used - Fair</option>
+                                <option value="New">New</option>
+                                <option value="Used - Great">Used - Great</option>
+                                <option value="Used - Good">Used - Good</option>
+                                <option value="Used - Fair">Used - Fair</option>
                             </select>
                     </div>
                 </div>
                 <div className='subsection'>
                     <h2 className='subheader'>Item Price</h2>
-                    <input id="price-input" type="text" onChange={handleChange}/>
+                    <input id="price-input" name="price" type="text" onChange={handleChange}/>
                 </div>
                 <button className='list-button' type="submit">List Item</button>
             </div>
