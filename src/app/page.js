cf9111e5fc1501acb13c6297b7d7fdb6e61ea043
Nicole Chung <nc06279@uga.edu>
2024-04-23 19:21:47 -0400

    category pages upd8

--- src/app/page.js
+++ src/app/page.js
@@ -5,19 +5,20 @@ import Category from './components/Category.js';
 import Cover from './components/Cover/Cover.js';
 import styles from './App.css';
 import User from '../../models/User.js';
+import Link from 'next/link';
 
 export default function Home() {
 
   return (
     <UserProvider>
       <main className="main">
-        <Header />
+        <Header/>
         <div className="content">
           <Cover/>
           <div className="category-main-container">
-            <Category link=" /women" src="/assets/WOMENS.png" title="WOMEN'S" />
-            <Category link=" /men" src="/assets/MENS.png" title="MEN'S" />
-            <Category link=" /home" src="/assets/HOME.png" title="HOME" />
+            <Category link="/women" src="/assets/WOMENS.png" title="WOMEN'S"/>
+            <Category link="/men" src="/assets/MENS.png" title="MEN'S" />
+            <Category link="/home" src="/assets/HOME.png" title="HOME" />
           </div>
         </div>
       </main>
